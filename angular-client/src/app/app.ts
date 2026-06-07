de4f26cbb9487343950c4b457479a3961bec55ec
Rajat Verma <cserajatverma@gmail.com>
2026-06-07 21:24:45 +0530

    Refactor app component to use Angular signals for title management and update style reference from app.scss to app.css.

--- angular-client/src/app/app.ts
+++ angular-client/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
 export class App {
-  protected title = 'twenty-one-game';
+  protected readonly title = signal('twenty-one-game');
 }
